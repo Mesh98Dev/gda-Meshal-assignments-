37a46e3ea5ddbee924fad213ea9df7e056cb2011
Mesh98Dev <Meshal.otaibi@outlook.sa>
2024-02-18 14:44:47 +0300

    Unfinished

--- Assets/PauseButtunClicked.cs
+++ Assets/PauseButtunClicked.cs
@@ -3,28 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.VisualScripting;
 
-public class PauseButtunClicked : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
-    //[SerializeField] GameObject m_PauseButtunClicked
+    public GameObject m_PauseMenu;
+    public bool isPaused;
     // Start is called before the first frame update
    
-   
+   void Start()
+    {
+       m_PauseMenu.SetActive(false);
+    }
+   void Update()
+{
+        if(Input.GetKey(KeyCode.Space))
+ {
+        if(isPaused)
+     {
+        ContinueGame();
+     }       
+        else if(isPaused)
+    {
+         PauseGame();
+    }
+ }
+}
    void OnMouseDown() {
 	PauseGame();
 }
 	
 private void PauseGame()
 {
-	Time.timeScale = 0;
-}  /* public void PauseGame()
+    m_PauseMenu.SetActive(true);
+	Time.timeScale = 0f;
+}
+    public void ContinueGame()
+{
+    m_PauseMenu.SetActive(false);
+    Time.timeScale = 1f;
+
+}
+/* public void PauseGame()
     {
         m_PauseButtunClicked
     }*/
 
     // Update is called once per frame
-   /* void Update()
-    {
-        
-    }*/
+   
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+    
+    private float speed;
+    public static int enemyKillCounter = 0;
+    private Rigidbody rb;
+    private Vector2 movement; 
+    
+    
     void Start()
     {
         
