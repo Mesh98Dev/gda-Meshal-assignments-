65cb5ecbc653e9a6424f2204b0aad1c31adfd1dc
Mesh98Dev <Meshal.otaibi@outlook.sa>
2024-02-25 18:33:33 +0300

    forgot to upload

---
+++ Assets/Scripts/ContRotation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContRotation : MonoBehaviour
+{ /*
+    public float magnitude;
+    public float speed;
+    Vector3 m_cube = new Vector3(1,0,0);
+    
+    
+
+     Update is called once per frame
+  void Update()
+    {
+       transform.localScale = new Vector3(sineAmount(),sineAmount(),sineAmount());
+      transform.position = new Vector3(sineAmount(),sineAmount(),sineAmount());
+        transform.Rotate(m_cube);
+        
+    }
+   public float sineAmount() 
+{
+         return magnitude * Mathf.Sin(Time.time * speed) ;   //movement += transform.right * Time.deltaTime * movementSpeed;
+     
+         transform.position = movement + Vector3.up * Mathf.Sin(Time.time * frequency) * magnitude;
+}*/
+}
