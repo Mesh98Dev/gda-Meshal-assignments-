744b2a11b604c7f29ab906d3d1b1a62e79fe2a05
Mesh98Dev <Meshal.otaibi@outlook.sa>
2024-02-26 11:20:26 +0300

    camera assignment

---
+++ Assets/Scripts/c3rdPersonCamera.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c3rdPersonCamera : MonoBehaviour
+{
+    public Transform target;
+    public float distance;
+    public float zoomSpeed;
+    // Start is called before the first frame update
+    /* void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check for mouse scroll wheel input
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        //Adjust the distance based on the scroll input
+        distance -= scrollInput * zoomSpeed;
+        // make sure the distance doesint go below min value
+        distance = Mathf.Clamp(distance, 2f, 10f); 
+        //update the camera position based on the new distance
+        UpdateCameraPosition();
+    } 
+    void UpdateCameraPosition()
+    {
+        //calcuate the new camera position based on the target andd the distance
+        Vector3 offset = new Vector3(0f, 0f, distance);
+        Vector3 newPosition = target.position + offset;
+        //update Camera position
+        transform.position = newPosition;
+        //make the camera look at the target
+        transform.LookAt(target.position);
+    }
+}
